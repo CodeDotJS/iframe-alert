1fe457e136941b318233f631301917e1f244f577
Rishi Giri <rushforlinux@gmail.com>
2015-12-08 10:42:32 +0530

    updated index

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 window.alert = function(text) {
+  var iframe = document.createElement('iframe');
+      iframe.setAttribute('text', '-');
+      iframe.style.display = 'none';
+      
+      document.documentElement.appendChild(iframe);
+      iframe.contentWindow.alert(text);
+      iframe.parentNode.removeChild(iframe);
+}
+
+alert('JavaScript');
